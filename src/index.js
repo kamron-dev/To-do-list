42cd43ed3b747326309c3f115e13008fd2480084
kamron-dev <k.kamron@yadnex.ru>
2024-01-10 17:22:13 +0500

    working to-dos obj with a working func to add to-dos

--- src/index.js
+++ src/index.js
@@ -2,4 +2,21 @@ import { toDosObj, addToDoObj } from "./to-dos";
 import { createToDo, priorities } from "./createToDo";
 
 
+const newDo = createToDo("Milk", "Buy some milk for the cat", "Some day", priorities.very);
+const anotherDo = createToDo("Writing", "Write some text to my book", "Today", priorities.not, true);
 
+
+addToDoObj(newDo, "home");
+addToDoObj(anotherDo, "default");
+
+
+console.table(toDosObj["home"]);
+console.table(toDosObj["default"]);
+
+console.log(toDosObj)
+
+
+
+// addToDo(createToDo("Milk", "Buy some milk for the cat", "Some day", priorities.very));
+// addToDo(createToDo("Reading", "Read the book that I recently bought", "This evening", priorities.important));
+// addToDo(createToDo("Water", "Buy some water", "Next week", priorities.not, true));
