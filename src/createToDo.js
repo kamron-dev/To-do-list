8377cb8a0bfa6abda07200fed289f8000c64aa5b
kamron-dev <k.kamron@yadnex.ru>
2024-01-10 17:55:53 +0500

    Evetyrhing in this branch working right

---
+++ src/createToDo.js
@@ -0,0 +1,31 @@
+//Function that creates to-dos
+const createToDo = (title, desc, dueDate, priority = "important", completed = false) => {
+    return {
+        id : Date.now(),
+        title,
+        desc,
+        dueDate,
+        priority, 
+        completed
+    }
+};
+
+// Object that holds priorities
+const priorities = {
+    "very": "very important",
+    "important": "important",
+    "not": "not important",
+};
+
+
+
+// addToDo(createToDo("Milk", "Buy some milk for the cat", "Some day", priorities.very));
+// addToDo(createToDo("Reading", "Read the book that I recently bought", "This evening", priorities.important));
+// addToDo(createToDo("Water", "Buy some water", "Next week", priorities.not, true));
+
+export { createToDo, priorities };
+
+
+
+
+
