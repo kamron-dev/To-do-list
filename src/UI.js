abdca862181d81cec0c56f210bb748369976e792
kamron-dev <k.kamron@yadnex.ru>
2024-01-11 14:36:56 +0500

    added the h1 title and centered it

--- src/UI.js
+++ src/UI.js
@@ -2,8 +2,6 @@ export default function renderUI() {
     const header = document.getElementById("header");
     const sidebar = document.getElementById("sidebar");
     const main = document.getElementById("main");
-
     
-
 
 }
