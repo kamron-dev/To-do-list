739376dd30a6a3471b0d605250ac906c5227e316
kamron-dev <k.kamron@yadnex.ru>
2024-01-12 17:31:02 +0500

    deleted the helper function from UI and created the buttons inside the function itself

--- src/UI.js
+++ src/UI.js
@@ -4,43 +4,48 @@ export function createMainDiv() {
     const mainDiv = document.createElement("div");
     mainDiv.setAttribute("id", "mainDiv");
     main.appendChild(mainDiv);
-
+    
     return main
 };
-// Function to render the projects to the DOM (into the projects-div), exported to use in index.js
+// Function to render the projects to the DOM as buttons (into the projects-div), exported to use in index.js
 export function renderProjectsToButtons(projects) {
     const projectsDiv = document.querySelector("#projects-div")
     const projectsKeys = Object.keys(projects);
 
-    // projectsKeys.forEach(key => {
-    //     const button = document.createElement("button");
-    //     button.textContent = key;
-    //     projectsDiv.appendChild(button);
-    // });
-    createButtons(projectsKeys, projectsDiv);
+    projectsKeys.forEach(key => {
+        const button = document.createElement("button");
+        button.classList.add("projects-list-button");
+        button.textContent = key;
+        projectsDiv.appendChild(button);
+    });
 
     return projectsDiv;
 };
 
 
-/* Helper function that creates buttons from object keys so 
-that the function above only has one responsibility (Solid) */
-function createButtons(keys, divToAppendTo) {
-    keys.forEach(key => {
-        const button = document.createElement("button");
-        button.textContent = key;
-        divToAppendTo.appendChild(button);
-    });
-    return divToAppendTo;
-};
-
-function createHeader(name) {
+// Function to create the mainDiv header that should be updated
+export function createHeader(name) {
     const main = document.getElementById("main");
     const mainHeader = document.createElement("h1");
     mainHeader.textContent = name;
-
+    mainHeader.setAttribute("id", "mainHeader");
     main.appendChild(mainHeader);
 
     return main;
 
-}
+};
+
+
+// export function displayProjects() {
+//     const buttons = document.querySelectorAll(".project-list-button");
+//     const mainHeader = document.querySelector("#mainHeader")
+//     buttons.forEach(button => {
+//         button.addEventListener("click", () => {
+//             console.log("clicked!")
+//             mainHeader.textContent = button.key;
+//         })
+//     })
+// };
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { toDosObj, addToDoObj, addNewProject } from "./to-dos";
 import { createToDo, priorities } from "./createToDo";
-import { createMainDiv, renderProjectsToButtons } from "./UI";
-import { alertDate } from "./dates";
+import { createMainDiv, renderProjectsToButtons, createHeader, displayProjects } from "./UI";
+// import { alertDate } from "./dates";
 
 // creating two new todos with createToDo function
 const newDo = createToDo("Milk", "Buy some milk for the cat", "Some day", priorities.very);
@@ -22,6 +22,8 @@ console.log(toDosObj)
 // rendering the projects from the object to the projects-div
 renderProjectsToButtons(toDosObj);
 
+// creating the header to the mainDiv
+createHeader("Lika i Ya");
 
 //rendering the mainDiv into the main 
 createMainDiv();
